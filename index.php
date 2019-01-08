ba5087759aff69303e212048a31ea456d0165851
Rifqi <iamstrelokivanovic@gmail.com>
2019-01-08 11:24:22 +0700

    Update

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
-$servername = "demowebcybermax.database.windows.net";
-$username = "rifqi";
-$password = "P@ssw0rd01!";
-
-$conn = new mysqli($servername, $username, $password);
-
-if($conn->connect_error){
-die("connection failed : " . $conn->connect_error);
-
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:demowebcybermax.database.windows.net,1433; Database = Demoweb-SQL", "rifqi", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
 }
 
-echo "connection success";
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "rifqi@demowebcybermax", "pwd" => "{your_password_here}", "Database" => "Demoweb-SQL", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:demowebcybermax.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 ?>
