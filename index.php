ae2753a0930287a50fa153c506aabfd80d6cdfab
strelok29 <iamstrelokivanovic@gmail.com>
2019-01-08 04:12:23 +0000

    adding file

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "rifqi";
+$password = "P@ssw0rd01!";
+
+$conn = new mysqli($servername, $username, $password);
+
+if($conn->connect_error){
+die("connection failed : " . $conn->connect_error);
+
+}
+
+echo "connection success";
+?>
