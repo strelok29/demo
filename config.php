1539c16a0ac8479d3b4f36b975da739a6e217677
Rifqi <iamstrelokivanovic@gmail.com>
2019-01-09 17:09:30 +0700

    update

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
  
 $databaseHost = 'localhost';
 $databaseName = 'demo';
-$databaseUsername = 'rifqi';
-$databasePassword = 'P@ssw0rd01!';
+$databaseUsername = 'root';
+$databasePassword = 'zhrijJe1NwQdALcyfDYzv6o6MmWia29BMrEmaZ7jT86a1QkEZWurWrAQ4xd4';
  
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
 ?>
