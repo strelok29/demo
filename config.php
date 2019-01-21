e6892662c1b59ed6e3148ba113fe2a224c9ace95
Rifqi <iamstrelokivanovic@gmail.com>
2019-01-21 11:33:19 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
  
-$databaseHost = 'cybermaxdemo.mysql.database.azure.com';
+$databaseHost = 'cybermaxdemo-mysqldbserver.mysql.database.azure.com';
 $databaseName = 'demo';
-$databaseUsername = 'rifqi@cybermaxdemo';
+$databaseUsername = 'rifqi@cybermaxdemo-mysqldbserver';
 $databasePassword = 'P@ssw0rd01!';
  
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
